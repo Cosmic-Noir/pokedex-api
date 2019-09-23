1d41a0d6bc7ef3aa51156de7b01392071e9e294a
Kristen Doucette <cosmicnoir90@gmail.com>
2019-09-23 17:23:42 -0600

    Add validation middleware for token authorization header

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 
@@ -5,9 +6,51 @@ const app = express();
 
 app.use(morgan("dev"));
 
-app.use((req, res) => {
-  res.send("Hello, world! This is ME!! And some more info!");
+console.log(process.env.API_TOKEN);
+
+const validTypes = [
+  `Bug`,
+  `Dark`,
+  `Dragon`,
+  `Electric`,
+  `Fairy`,
+  `Fighting`,
+  `Fire`,
+  `Flying`,
+  `Ghost`,
+  `Grass`,
+  `Ground`,
+  `Ice`,
+  `Normal`,
+  `Poison`,
+  `Psychic`,
+  `Rock`,
+  `Steel`,
+  `Water`
+];
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+
+  next();
 });
+
+function handleGetTypes(req, res) {
+  res.json(validTypes);
+}
+
+app.get("/types", handleGetTypes);
+
+function handleGetPokemon(req, res) {
+  res.send("Hello, Pokemon!");
+}
+
+app.get("/pokemon", handleGetPokemon);
 
 const PORT = 8000;
 
